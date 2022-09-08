600f4062996b8607e8733d30d35ec8332c9e783f
Addison <S10222525@connect.np.edu.sg>
2022-09-08 18:38:28 +0800

    style: update card item UI

--- incognito/lib/card_item.dart
+++ incognito/lib/card_item.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:incognito/main.dart';
+import 'package:incognito/user.dart';
 
 class MyCard extends StatelessWidget {
+  final User user;
+  MyCard(this.user);
+  var index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(10),
-      width: double.infinity,
-      height: 100,
-      color: const Color.fromARGB(255, 37, 37, 37),
-      child: const Text("Hello", style: TextStyle(color: Colors.white)),
-    );
+        child: Padding(
+      padding: EdgeInsets.all(10),
+      child: Card(
+        color: const Color.fromARGB(255, 41, 41, 41),
+        margin: const EdgeInsets.all(5),
+        child: ListTile(
+          title:
+              Text("${user.name}", style: const TextStyle(color: Colors.white)),
+          subtitle: Text(
+              "${user.username} | ${user.password} | ${FormatDate(user.created)}",
+              style: const TextStyle(color: Colors.white)),
+          leading: const Icon(Icons.person, color: Colors.white, size: 50),
+          trailing: const Icon(Icons.arrow_forward_ios, color: Colors.white),
+          contentPadding: const EdgeInsets.all(10),
+          onTap: () {
+            print("${user.name} Clicked");
+          },
+        ),
+      ),
+    ));
+  }
+
+  String FormatDate(DateTime date) {
+    return "${date.day}/${date.month}/${date.year}";
   }
 }
