6de5bdfee12e85db9a0b51352ec340ac054dfbfc
Addison <S10222525@connect.np.edu.sg>
2022-09-08 18:39:28 +0800

    refactor: move add button - Moved add button from main.dart to index.dart

--- incognito/lib/index.dart
+++ incognito/lib/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:incognito/card_item.dart';
+import 'package:incognito/user.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -9,17 +10,27 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
-  final userList = [
-    "User1",
-  ];
+  var userList = [];
+
+  final index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-            itemCount: userList.length,
-            itemBuilder: (context, index) {
-              return MyCard();
-            }));
+      backgroundColor: Color.fromARGB(255, 27, 27, 27),
+      body: ListView.builder(
+          itemCount: userList.length,
+          itemBuilder: (context, index) {
+            return MyCard(userList[index]);
+          }),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.person_add),
+          onPressed: () {
+            print("Clicked - Adding New User");
+            setState(() {
+              userList.add(User("User ${userList.length + 1}"));
+            });
+          }),
+    );
   }
 }
